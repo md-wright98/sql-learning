0ae66d9ff2de8ca641d790c5ca30e5aeba7647dd
Michael Wright <101717530+md-wright98@users.noreply.github.com>
2025-05-30 15:02:34 +0100

    Create 1661. Average Time of Process per Machine.sql

---
+++ leetcode/sql-50/1661. Average Time of Process per Machine.sql
@@ -0,0 +1,5 @@
+## 1661. Average Time of Process per Machine
+## https://leetcode.com/problems/average-time-of-process-per-machine/description/?envType=study-plan-v2&envId=top-sql-50
+
+## Problem: Find the average processing time each machine takes to complete a task using the start and end timestamps
+
