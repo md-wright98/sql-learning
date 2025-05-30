e6a858920bbbb8ea132c3509c7e625704e084160
Michael Wright <101717530+md-wright98@users.noreply.github.com>
2025-05-30 11:03:01 +0100

    Create 1757. Recyclable and Low Fat Products.sql

---
+++ leetcode/sql-50/1757. Recyclable and Low Fat Products.sql
@@ -0,0 +1,8 @@
+## 1757. Recyclable and Low Fat Products  
+## 🔗 [LeetCode #1757](https://leetcode.com/problems/recyclable-and-low-fat-products/)
+
+## Problem: Write a solution to find the ids of products that are both low fat and recyclable.
+
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y'
