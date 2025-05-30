98171a0d90bec4d31c26a21b52258eec07555e17
Michael Wright <101717530+md-wright98@users.noreply.github.com>
2025-05-30 14:59:08 +0100

    Create 197. Rising Temperature.sql

---
+++ leetcode/sql-50/197. Rising Temperature.sql
@@ -0,0 +1,5 @@
+## 197. Rising Temperature
+## https://leetcode.com/problems/rising-temperature/?envType=study-plan-v2&envId=top-sql-50
+
+## Problem: return the IDs of all the dates where the temperature was higher than the day before
+
