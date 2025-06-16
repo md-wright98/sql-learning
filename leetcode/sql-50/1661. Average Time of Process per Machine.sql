7e0b8aa5cf67f40bb5b27f2c937a6ef571e50de0
Michael Wright <101717530+md-wright98@users.noreply.github.com>
2025-06-16 19:59:02 +0100

    Update 1661. Average Time of Process per Machine.sql

--- leetcode/sql-50/1661. Average Time of Process per Machine.sql
+++ leetcode/sql-50/1661. Average Time of Process per Machine.sql
@@ -2,7 +2,7 @@
 ## https://leetcode.com/problems/average-time-of-process-per-machine/description/?envType=study-plan-v2&envId=top-sql-50
 
 ## Problem: Find the average processing time each machine takes to complete a task using the start and end timestamps, rounded to 3 decimal
-            places
+##          places
 
 SELECT machine_id,
        ROUND(AVG(processing_time), 3) AS processing_time
