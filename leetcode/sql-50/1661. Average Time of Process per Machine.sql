cc414c660632a5d93cace03c5093283146b0234a
Michael Wright <101717530+md-wright98@users.noreply.github.com>
2025-06-16 19:58:31 +0100

    Update 1661. Average Time of Process per Machine.sql

--- leetcode/sql-50/1661. Average Time of Process per Machine.sql
+++ leetcode/sql-50/1661. Average Time of Process per Machine.sql
@@ -3,3 +3,14 @@
 
 ## Problem: Find the average processing time each machine takes to complete a task using the start and end timestamps
 
+SELECT machine_id,
+       ROUND(AVG(processing_time), 3) AS processing_time
+FROM (SELECT machine_id, 
+       process_id, 
+       ROUND(MAX(timestamp) - MIN(timestamp), 3) AS processing_time
+FROM Activity
+GROUP BY machine_id, process_id) as subquery
+GROUP BY machine_id
+
+## use a subquery to create a table containing the processing time for each process, grouped by machine and process id, and then use this
+## to calculate the average processing time of all the processes per machine by grouping by machine_id
