2e87b623fa79c9218638dcb19647f5835e97390d
Michael Wright <101717530+md-wright98@users.noreply.github.com>
2025-06-16 19:48:10 +0100

    Update 197. Rising Temperature.sql

--- leetcode/sql-50/197. Rising Temperature.sql
+++ leetcode/sql-50/197. Rising Temperature.sql
@@ -3,3 +3,11 @@
 
 ## Problem: return the IDs of all the dates where the temperature was higher than the day before
 
+SELECT w1.id AS Id
+FROM weather AS w1
+JOIN weather AS w2
+    ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature
+
+## use DATEDIFF to perform a self join, joining rows that are one day apart and then filtering for days where the temperature is
+## larger than the day before
