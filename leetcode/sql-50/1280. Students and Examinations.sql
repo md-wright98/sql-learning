22ae24423a1153d0182a3a6529035bfb41ba5746
Michael Wright <101717530+md-wright98@users.noreply.github.com>
2025-06-17 17:36:41 +0100

    Update 1280. Students and Examinations.sql

--- leetcode/sql-50/1280. Students and Examinations.sql
+++ leetcode/sql-50/1280. Students and Examinations.sql
@@ -15,3 +15,20 @@ GROUP BY st.student_name, ex.subject_name
 ORDER BY student_id, subject_name
 
 ## ISSUE: every student took all three subjects, and this query does not return students who attended 0 exams for a subject, only those who did attend
+
+## 2nd Attempt:
+
+SELECT stu.student_id, 
+       stu.student_name, 
+       sub.subject_name,
+       COUNT(exa.student_id) AS attended_exams
+FROM Students AS stu
+CROSS JOIN Subjects AS sub
+LEFT JOIN Examinations AS exa
+    ON stu.student_id = exa.student_id 
+    AND sub.subject_name = exa.subject_name
+GROUP BY stu.student_id, sub.subject_name
+ORDER BY stu.student_id, sub.subject_name
+
+## At the time I was not aware of CROSS JOIN, which facilitates creating a table that contains every combination of student and subject.
+## Joining on two fields is then essential to get the right groupings in the GROUP BY clause
